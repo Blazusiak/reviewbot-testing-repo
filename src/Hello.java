7eaca153e87634a0f682f335510e5ff3bab441ca
Jacob Blazusiak <jblazusi@ualberta.ca>
2020-10-09 14:10:00 -0600

    Initialized for demo

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ package oata;
 
 public class Hello {
     public static void main(String[] args) {
-        String s = null;
-        s.length();
+        //String s = null;
+        //s.length();
     }
 }
